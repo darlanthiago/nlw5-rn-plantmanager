1ce1054ee5d22de1e6c3fe4c2765fc301d411f0c
Darlan Thiago <tdarlan1@hotmail.com>
2021-04-20 23:28:55 -0300

    Add Navigation

--- App.tsx
+++ App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-import Welcome from "./src/pages/Welcome";
+import AppLoading from "expo-app-loading";
+
+// import Welcome from "./src/pages/Welcome";
+// import UserIdentification from "./src/pages/UserIdentification";
+// import Confirmation from "./src/pages/Confirmation";
+
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+} from "@expo-google-fonts/jost";
+import Routes from "./src/routes";
 
 export default function App() {
-  return <Welcome />;
+  let [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return <Routes />;
 }
